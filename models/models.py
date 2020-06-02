d048c465484aae40febd423a65cbdd29e31cf1da
block-cat <kfx2007@outlook.com>
2020-06-02 02:41:55 +0000

    13.0

--- models/models.py
+++ models/models.py
@@ -15,7 +15,7 @@ class mail_thread(models.AbstractModel):
     _inherit = "mail.thread"
 
     @api.model
-    def _get_tracked_fields(self, updated_fields):
+    def _get_tracked_fields(self):
         if self.env['ir.model']._get(self._name).track:
             fields = self.fields_get()
             dels = [f for f in fields if f in models.LOG_ACCESS_COLUMNS or f.startswith('_') or f == 'id']
@@ -23,4 +23,4 @@ class mail_thread(models.AbstractModel):
                 del fields[x]
             return fields
         else:
-            super(mail_thread, self)._get_tracked_fields(updated_fields)
+            super(mail_thread, self)._get_tracked_fields()
