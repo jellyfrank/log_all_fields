02c2faf66c1141604281dad6ba3c33a5e0ddc239
block-cat <kfx2007@outlook.com>
2021-06-05 07:28:21 +0000

    [FIX]适配14.0

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, _, tools
 
 
 class ir_model(models.Model):
@@ -14,13 +14,13 @@ class mail_thread(models.AbstractModel):
 
     _inherit = "mail.thread"
 
-    @api.model
+    @tools.ormcache('self.env.uid', 'self.env.su')
     def _get_tracked_fields(self):
         if self.env['ir.model']._get(self._name).track:
             fields = self.fields_get()
             dels = [f for f in fields if f in models.LOG_ACCESS_COLUMNS or f.startswith('_') or f == 'id']
             for x in dels:
                 del fields[x]
-            return fields
+            return set(fields)
         else:
-            super(mail_thread, self)._get_tracked_fields()
+            return super(mail_thread, self)._get_tracked_fields()
